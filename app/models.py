912983e48a119a53bca221968917efe5d22e34b6
Daniel <codedbydaniel@gmail.com>
2025-12-14 10:20:54 +0630

    foreignkey

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class PostModel(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
